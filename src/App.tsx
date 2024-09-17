ee939ad6c5dee75a6ddc4c38155e22e7cb1a482d
ww1659 <ww1659@my.bristol.ac.uk>
2024-09-17 11:04:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
