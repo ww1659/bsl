5fefb91ddc8bb5f5fc8b412ab51105f1133307bc
ww1659 <ww1659@my.bristol.ac.uk>
2024-09-20 10:30:35 +0100

    initialised hook and supabase client

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { Button } from "./components/ui/button";
+import { useFetchCustomers } from "./hooks/useFetchCustomers";
 
 function App() {
+  const { data, isLoading, isError, error } = useFetchCustomers();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
   return (
     <>
       <h1>Vite + React</h1>
       <Button>Click Me</Button>
+      <div>
+        <h1>Customers</h1>
+        <ul>
+          {data.map((customer) => (
+            <li key={customer.id}>{customer.customer_name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
---
+++ src/hooks/useFetchCustomers.ts
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// Fetch data with Supabase
+const fetchCustomers = async () => {
+  const { data, error } = await supabase
+    .from('customers')
+    .select('*');
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  return data;
+};
+
+// Hook that wraps the query in React Query
+export const useFetchCustomers = () => {
+  return useQuery({queryKey: ['customers'], queryFn: fetchCustomers});
+};
+
